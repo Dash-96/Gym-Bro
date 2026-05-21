ef126478f9b1de43e8228d266c7bec5152b3951f
David-Shmidt <davidshmidt@gmail.com>
2026-05-21 21:35:31 +0300

    config chnages

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,22 @@
+const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 config.resolver.sourceExts.push("sql");
+config.resolver.assetExts.push("wasm");
+
+const signalrBrowserBundle = path.resolve(
+  __dirname,
+  "node_modules/@microsoft/signalr/dist/browser/signalr.js",
+);
+const originalResolveRequest = config.resolver.resolveRequest;
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (platform === "web" && moduleName === "@microsoft/signalr") {
+    return { type: "sourceFile", filePath: signalrBrowserBundle };
+  }
+  return originalResolveRequest
+    ? originalResolveRequest(context, moduleName, platform)
+    : context.resolveRequest(context, moduleName, platform);
+};
+
 module.exports = config;
